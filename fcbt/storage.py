f5f5da8896b0312ee4a5bb1f6e0292c1366c4da9
Thomas <126467625+thomascrecy@users.noreply.github.com>
2026-03-25 21:50:51 +0100

    salt

--- fcbt/storage.py
+++ fcbt/storage.py
@@ -1,19 +1,28 @@
 import json
+import os
 from dataclasses import asdict
-from fcbt.crypto import encrypt, decrypt
+from fcbt.crypto import encrypt, decrypt, derive_key
 from fcbt.models import Vault, Entry
 
-def save_vault(vault, key, file_name: str):
+def save_vault(vault, password, file_name: str):
+    salt = os.urandom(16)
     dico = asdict(vault)
     text = json.dumps(dico)
-    encrypted_text = encrypt(text, key)
+    salted_key = derive_key(password, salt)
+    encrypted_text = encrypt(text, salted_key)
+    
     with open(file_name, "wb") as f:
-        f.write(encrypted_text)
+        f.write(salt + encrypted_text)
 
-def load_vault(key, file_name: str):
+def load_vault(password, file_name: str):
     with open(file_name, "rb") as f:
-        encrypted_text = f.read()
+        data = f.read()
+    salt = data[:16]
+    encrypted_text = data[16:]
+    
+    key = derive_key(password, salt)
     decrypted_text = decrypt(encrypted_text, key)
+    
     dico = json.loads(decrypted_text)
     vault = Vault(entries=[Entry(**entry) for entry in dico["entries"]])
     return vault
